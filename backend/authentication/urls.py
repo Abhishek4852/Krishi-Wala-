2225d72e8b4663920baaae3aa5226b84fd923cf5
Geetanshi Jain <geetanshi@gmail.com>
2026-06-15 22:29:06 +0530

    Update authentication routes to use v2 API

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Registration, name='Registration'),
-    path('register/', views.Registration, name='Registration_alias'),
-    path('login/', views.login, name="login"),
-    path("token_validation/", views.token_validation, name="token_validation"),
-    path("load-db/", views.load_db_from_json, name="load_db_from_json"),
+    path('v2/', views.Registration, name='Registration'),
+    path('v2/register/', views.Registration, name='Registration_alias'),
+    path('v2/login/', views.login, name="login"),
+    path("v2/token_validation/", views.token_validation, name="token_validation"),
+    path("v2/load-db/", views.load_db_from_json, name="load_db_from_json"),
 ]
