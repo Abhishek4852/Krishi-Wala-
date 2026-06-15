21809bff9de4a2bc442ea03c2fc57a5a105d9f6e
Abhishek yaduwanshi <abhishekyaduwanshi4852@gmail.com>
2026-06-15 16:32:36 +0530

    base url updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://krishi-wala-6im8.onrender.com";
+export const API_BASE_URL = "https://krishi-wala-6im8.onrender.com";
+  // import.meta.env.VITE_API_BASE_URL || "https://krishi-wala-6im8.onrender.com";
